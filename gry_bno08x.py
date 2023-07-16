889e5b51fd39c54c6d18c49bad0a951512e49b95
garid3000 <garidzorigoo@gmail.com>
2023-07-16 21:43:56 +0900

    gry_bno08x: fix optional-type variable checking

--- gry_bno08x.py
+++ gry_bno08x.py
@@ -1,3 +1,5 @@
+from typing import Optional, Tuple
+
 import time
 import board
 import busio
@@ -23,23 +25,25 @@ bno.enable_feature(adafruit_bno08x.BNO_REPORT_RAW_GYROSCOPE)
 bno.enable_feature(adafruit_bno08x.BNO_REPORT_RAW_MAGNETOMETER)
 
 
-data = np.zeros((100, 9))
+data = np.zeros((100, 9), dtype=np.float64)
 
-# while True:
 for i in range(100):
     time.sleep(0.1)
 
-    accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
-    gyro_x,  gyro_y,  gyro_z = bno.gyro           # pylint:disable=no-member
-    mag_x,   mag_y,   mag_z = bno.magnetic        # pylint:disable=no-member
+    acc_xyz: Optional[Tuple[float, float, float]] = bno.acceleration
+    gyr_xyz: Optional[Tuple[float, float, float]] = bno.gyro
+    mag_xyz: Optional[Tuple[float, float, float]] = bno.magnetic
 
-    print(f"X={accel_x:8.6f} Y={accel_y:8.6x} Z={accel_z:8.6x} |", endl='')
-    print(f"X={gyro_x:8.6f}  Y={gyro_y:8.6x}  Z={gyro_z:8.6x}  |", endl='')
-    print(f"X={mag_x:8.6f}   Y={mag_y:8.6x}   Z={mag_z:8.6x}   |", endl='')
+    if acc_xyz is not None:
+        print(f"X={acc_xyz[0]:8.6f} Y={acc_xyz[1]:8.6x} Z={acc_xyz[2]:8.6x} |", end='')
+        data[i, 0:3] = acc_xyz
+    if gyr_xyz is not None:
+        print(f"X={gyr_xyz[0]:8.6f} Y={gyr_xyz[1]:8.6x} Z={gyr_xyz[2]:8.6x} |", end='')
+        data[i, 3:6] = gyr_xyz
+    if mag_xyz is not None:
+        print(f"X={mag_xyz[0]:8.6f} Y={mag_xyz[1]:8.6x} Z={mag_xyz[2]:8.6x} |", end='')
+        data[i, 6:9] = mag_xyz
 
-    data[i, :] = [accel_x, accel_y, accel_z,
-                  gyro_x,  gyro_y,  gyro_z,
-                  mag_x,   mag_y,   mag_z]
     if bno.shake:
         print("SHAKE DETECTED!")
 
