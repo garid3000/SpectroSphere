6bae9e27ffada442f36a91c54fbc3b9719c11ce8
garid3000 <garidzorigoo@gmail.com>
2024-06-20 23:06:58 +0900

    Fix Gry Bno file

---
+++ custom_libs/sscan1.py
@@ -0,0 +1,214 @@
+import sys
+from typing import Tuple
+import serial
+# import re
+import time
+
+# _start     = "J"
+# _stop      = "K"
+# _hardstop  = "L"
+
+class Sscan:
+    def __init__(self, devname, baudrate, timeout):
+        self.dbg = True
+        self.ser = serial.Serial(devname, baudrate=baudrate, timeout=timeout)
+    def set_pos(self, d1, d2):
+        self.set_pos_deg( 1, d1)
+        self.set_pos_deg( 2, d2)
+
+    def get_pos(self):
+        print("pos:", self.get_pos_deg(1), self.get_pos_deg(2))
+
+    def goto(self, d1, d2, wait = False):
+        self.ax1_goto(d1)
+        self.ax2_goto(d2)
+
+        if wait:
+            self.wait2stop(1)
+            self.wait2stop(2)
+
+    def ax1_goto(self, degree, wait = False):
+        self.stop(1)
+        fut_pos = self.deg2step(degree)
+        cur_pos = self.inq_pos(1)
+        self.set_motion_mode(1, fut_pos<cur_pos)
+        self.set_target_deg(1, degree)
+        self.start(1)
+
+        if wait:
+            self.wait2stop(1)
+
+
+    def ax2_goto(self, degree, wait = False):
+        self.stop(2)
+        fut_pos = self.deg2step(degree)
+        cur_pos = self.inq_pos(2)
+        self.set_motion_mode(2, fut_pos<cur_pos)
+        self.set_target_deg(2, degree)
+        self.start(2)
+        if wait:
+            self.wait2stop(2)
+
+
+    def set_target_deg(self, axis, deg):
+        hexval = self.deg2step(deg)
+        ihexval = self.invertHex(hexval)
+        print("S{}{:06X}".format(axis, ihexval))
+        self.send("S{}{:06X}".format(axis, ihexval))
+
+    def set_pos_deg(self, axis, deg):
+        hexval = self.deg2step(deg)
+        ihexval = self.invertHex(hexval)
+        #print("E{}{:06X}".format(axis, ihexval))
+        self.send("E{}{:06X}".format(axis, ihexval))
+
+    def invertHex(self, hexval):
+        newhex  = (hexval & (0xFF    ) ) << 16
+        newhex |= (hexval & (0xFF <<8) )
+        newhex |= (hexval & (0xFF <<16)) >> 16
+        #print("inv {:06X}".format(newhex))
+        #print("act {:06X}".format(hexval))
+        return newhex
+
+    def set_motion_mode(self, axis, cw):
+        self.wait2stop(axis)
+        if cw:
+            self.send("G{}01".format(axis))
+            print("G{}01".format(axis))
+        else:
+            self.send("G{}00".format(axis))
+            print("G{}00".format(axis))
+
+
+
+    def get_pos_deg(self, axis):
+        return self.step2deg(self.inq_pos(axis))
+
+    def get_target_deg(self, axis):
+        return self.step2deg(self.inq_target(axis))
+    def inq_pos(self, axis):
+        _, val = self.send('j{}'.format(axis))
+        return val - 0x800000
+
+    def inq_target(self, axis):
+        _, val = self.send('h{}'.format(axis))
+        return val - 0x800000
+
+    def deg2step(self, degree):
+        return int(degree*0x1FA400/360) +0x800000
+
+    def step2deg(self, step):
+        return step/0x1FA400*360
+
+    def stop(self, axis=3, wait = False):
+        self.send('K{}'.format(axis))
+        if wait:
+            self.wait2stop(1)
+            self.wait2stop(2)
+
+    def start(self, axis=3, wait = False):
+        self.send('J{}'.format(axis))
+        if wait:
+            self.wait2stop(1)
+            self.wait2stop(2)
+
+
+    def wait2stop(self, axis, delay = .5):
+        while 1:
+            if self.isStopped(axis):
+                break
+            time.sleep(delay)
+
+
+    def isStopped(self, axis):
+        self.ser.reset_input_buffer()
+        self.ser.write(':f{}\r'.format(axis).encode())
+        success = self.ser.read(1)
+        val = 0
+        #print(success)
+        if success == b'=':
+            _count_ = 0
+            while 1:
+                retchar = self.ser.read(1)
+                #print(retchar, type(retchar), "{:X}".format(val))
+                if retchar == b'\r':
+                    return False
+
+                if retchar in b"1234567890ABCDEFabcdef":
+                    val |= (int(retchar, 16) << _count_ * 4)
+                    if _count_ == 1:
+                        if int(retchar, 16) %2 ==0:
+                            return True
+
+                else:
+                    wrongcharflag = False # type: ignore
+                _count_ += 1
+        else:
+            return False
+
+
+    #def send(self, cmd: str, retsize=1):
+    def send(self, cmd: str) -> Tuple[bool, int]:
+        seq = [1,0,3,2,5,4]
+        self.ser.reset_input_buffer()
+        self.ser.write(':{}\r'.format(cmd).encode())
+        success = self.ser.read(1)
+        val = 0
+        #print(success)
+        if success == b'=':
+            _count_ = 0
+            while 1:
+                retchar = self.ser.read(1)
+                #print(retchar, type(retchar), "{:X}".format(val))
+                if retchar == b'\r':
+                    return True, val
+
+                if retchar in b"1234567890ABCDEFabcdef":
+                    val |= (int(retchar, 16) << seq[_count_] * 4)
+                else:
+                    wrongcharflag = True # type: ignore
+                _count_ += 1
+        #else:
+        #    print('some error', val, success)
+        #    return False, 0
+        print('some error', val, success)
+        return False, 0
+
+
+
+
+if __name__ == "__main__":
+    cli_args = {
+        each_arg.split('=')[0]:each_arg.split('=')[1]
+        for each_arg in sys.argv[1:]
+        if each_arg.count('=') == 1
+    }
+
+    s = Sscan('/dev/ttyUSB0' if 'dev' not in cli_args else cli_args['dev'] , baudrate=9600, timeout=0.02)
+
+    def test():
+        for _ in range(5):
+            s.goto(360, 360, wait=False)
+            while True:
+                if abs(s.get_pos_deg(1) - 360) + abs(s.get_pos_deg(2) - 360) < 1:
+                    break
+                print(s.get_pos_deg(1), s.get_pos_deg(2))
+            s.goto(-360, -360, wait=False)
+            while True:
+                if abs(s.get_pos_deg(1) + 360) + abs(s.get_pos_deg(2) + 360) < 1:
+                    break
+                print(s.get_pos_deg(1), s.get_pos_deg(2))
+    #def test():
+    #    s.goto(-360, -90, True)
+    #    s.goto(+360, +90, True)
+    #    s.goto(0, 0, True)
+
+    desired_azi = s.get_pos_deg(1) if "azi" not in cli_args else float(cli_args["azi"])
+    desired_elv = s.get_pos_deg(2) if "elv" not in cli_args else float(cli_args["elv"])
+    waiting = False if "wait" not in cli_args else True
+
+    s.goto(desired_azi, desired_elv, waiting)
+
+    if "set" in cli_args:
+        if cli_args["set"] == "0":
+            s.set_pos(0, 0)
--- gry_bno08x.py
+++ gry_bno08x.py
@@ -1,81 +1,57 @@
-import sys
 import time
+import sys
 import board
 import busio
-import adafruit_bno08x
 from adafruit_bno08x.i2c import BNO08X_I2C
+from adafruit_bno08x import (
+    BNO_REPORT_ACCELEROMETER,
+    BNO_REPORT_GYROSCOPE,
+    BNO_REPORT_MAGNETOMETER,
+    BNO_REPORT_ROTATION_VECTOR,
+)
 import numpy as np
-i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
+
+
+i2c = busio.I2C(board.SCL, board.SDA)  # , frequency=100000)
 bno = BNO08X_I2C(i2c)
-
-bno.enable_feature(adafruit_bno08x.BNO_REPORT_ACCELEROMETER)
-bno.enable_feature(adafruit_bno08x.BNO_REPORT_GYROSCOPE)
-bno.enable_feature(adafruit_bno08x.BNO_REPORT_MAGNETOMETER)
-bno.enable_feature(adafruit_bno08x.BNO_REPORT_LINEAR_ACCELERATION)
-bno.enable_feature(adafruit_bno08x.BNO_REPORT_ROTATION_VECTOR)
-bno.enable_feature(adafruit_bno08x.BNO_REPORT_GEOMAGNETIC_ROTATION_VECTOR)
-bno.enable_feature(adafruit_bno08x.BNO_REPORT_GAME_ROTATION_VECTOR)
-bno.enable_feature(adafruit_bno08x.BNO_REPORT_STEP_COUNTER)
-bno.enable_feature(adafruit_bno08x.BNO_REPORT_STABILITY_CLASSIFIER)
-bno.enable_feature(adafruit_bno08x.BNO_REPORT_ACTIVITY_CLASSIFIER)
-bno.enable_feature(adafruit_bno08x.BNO_REPORT_SHAKE_DETECTOR)
-bno.enable_feature(adafruit_bno08x.BNO_REPORT_RAW_ACCELEROMETER)
-bno.enable_feature(adafruit_bno08x.BNO_REPORT_RAW_GYROSCOPE)
-bno.enable_feature(adafruit_bno08x.BNO_REPORT_RAW_MAGNETOMETER)
-
-
-# data = np.zeros((100, 9))
-# 
-# for i in range(100):
-#     time.sleep(0.1)
-#     accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
-#     #print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z), end = '|')
-# 
-#     gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
-#     #print("X: %0.6f  Y: %0.6f Z: %0.6f rads/s" % (gyro_x, gyro_y, gyro_z), end='|')
-# 
-#     mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
-#     #print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z), end='|')
-# 
-#     data[i,:] = [accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z,mag_x, mag_y, mag_z   ]
-#     #if bno.shake:
-#     #    print("SHAKE DETECTED!")
-#     #print('', end='\r', flush=True)
-
+bno.enable_feature(BNO_REPORT_ACCELEROMETER)
+bno.enable_feature(BNO_REPORT_GYROSCOPE)
+bno.enable_feature(BNO_REPORT_MAGNETOMETER)
+bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
 
 
 if __name__ == "__main__":
-    cli_args = {
-        each_arg.split('=')[0]:each_arg.split('=')[1]
-        for each_arg in sys.argv[1:]
-        if each_arg.count('=') == 1
-    }
-    
-    
+    cli_args = {each_arg.split("=")[0]: each_arg.split("=")[1] for each_arg in sys.argv[1:] if each_arg.count("=") == 1}
+
     if "out" in cli_args:
         data = np.zeros((100, 9))
 
         for i in range(100):
             time.sleep(0.1)
-            accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
-            #print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z), end = '|')
-
-            gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
-            #print("X: %0.6f  Y: %0.6f Z: %0.6f rads/s" % (gyro_x, gyro_y, gyro_z), end='|')
-
+            acc_x, acc_y, acc_z = bno.acceleration  # pylint:disable=no-member
+            gyr_x, gyr_y, gyr_z = bno.gyro  # pylint:disable=no-member
             mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
-            #print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z), end='|')
-
-            data[i,:] = [accel_x, accel_y, accel_z, gyro_x, gyro_y, gyro_z,mag_x, mag_y, mag_z   ]
+            data[i, :] = [acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z, mag_x, mag_y, mag_z]
 
         np.save(f"{cli_args['out']}", data)
 
     elif "show_loop" in cli_args:
         for i in range(int(cli_args["show_loop"])):
             time.sleep(0.1)
-            accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
-            print(f"X:{accel_x:0.3f} Y:{accel_y:0.3f} Z:{accel_z:0.3f} m/s^2")
+            acc_x, acc_y, acc_z = bno.acceleration  # pylint:disable=no-member
+            print(f"X:{acc_x:0.3f} Y:{acc_y:0.3f} Z:{acc_z:0.3f} m/s^2")
 
     else:
-        accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
-        print(f"X:{accel_x:0.3f}\nY:{accel_y:0.3f}\nZ:{accel_z:0.3f} m/s^2")
+        while True:
+            acc_x, acc_y, acc_z = bno.acceleration  # pylint:disable=no-member
+            print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (acc_x, acc_y, acc_z), end="\t")
+
+            gyr_x, gyr_y, gyr_z = bno.gyro  # pylint:disable=no-member
+            print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (gyr_x, gyr_y, gyr_z), end="\t")
+
+            mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
+            print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (mag_x, mag_y, mag_z), end="\t")
+            quat_i, quat_j, quat_k, quat_real = bno.quaternion
+            print(quat_i, quat_j, quat_k, quat_real)
+
+            time.sleep(0.05)
