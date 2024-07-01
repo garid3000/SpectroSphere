a66b9ddfe5e2142979dfdddd13db96338a82a415
garid3000 <garidzorigoo@gmail.com>
2024-07-02 03:29:44 +0900

    Camera check update in order to avoid issue with webcam output

--- check_cam.py
+++ check_cam.py
@@ -2,14 +2,12 @@ import cv2
 import numpy as np
 
 cap0 = cv2.VideoCapture(0)
-cap2 = cv2.VideoCapture(2)
-
-
 r, f = cap0.read()
 if r:
     np.save("/tmp/cam0.npy", f)
-    #cv2.imwrite("/tmp/cam0.png", f)
+cap0.release()
 
+cap2 = cv2.VideoCapture(2)
 r, f = cap2.read()
 if r:
     np.save("/tmp/cam2.npy", f)
