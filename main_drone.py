4b6f7dba4e568b241860fc7fc63463b4df6cc29e
garid z <garidzorigoo@gmail.com>
2024-08-06 01:11:13 +0900

    printing

--- main_drone.py
+++ main_drone.py
@@ -85,4 +85,5 @@ while (time.time() - t0 < duration):
     dBuf_img0[count, :, :] = cap0.read()[:, 200:400, 0]
     dBuf_img1[count, :, :] = cap2.read()[:, :, 0]      
     dBuf_ori[count, -1] = time.time() - t0
+    print(count, time.time() - t0, "of", duration)
     count += 1
